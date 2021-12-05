119ea48683d286c6d7ee0d7a16b2cee2b0e18790
JessieGumanab <Jessiegumanab@mail.fresnostate.edu>
2021-12-05 12:35:08 -0800

    added sections for login and bills to start

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BillsComponent } from './bills/bills.component';
 import { MainPageComponent } from './main-page/main-page.component';
-
+import { LoginComponent } from './login/login.component';
 const routes: Routes = [
   {
     path:'', component: MainPageComponent
-  }
+  },
+  {
+    path:'login', component: LoginComponent
+  },
+  {
+    path:'bills', component: BillsComponent
+  },
+  
 ];
 
 @NgModule({
